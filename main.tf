fbec4ceeb9ccbb9b813607cd649e5cb5e2c21b1b
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-09-30 18:47:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ variable "private_subnet_count" {
   default     = 2
 }
 
-}
+
 variable "public_subnet_cidr_blocks" {
   type        = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
