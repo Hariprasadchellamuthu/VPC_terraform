92c58cb8261c3a3b0ca02943e4e99da5dee1370d
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-09-30 18:41:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,23 +6,26 @@ variable "vpc_cidr_block" {
   type        = string
   default = "10.0.0.0/16"
 }
-variable "public_subnet_cidrs" {
- type        = list(string)
- description = "Public Subnet CIDR values"
- default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+variable "public_subnet_count" {
+  type        = number
+  description = "Number of public subnets"
+  default     = 2
 }
-variable "private_subnet_cidrs" {
- type        = list(string)
- description = "Private Subnet CIDR values"
+
+variable "private_subnet_count" {
+  type        = number
+  description = "Number of private subnets"
+  default     = 2
+}
  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 variable "public_subnet_cidr_blocks" {
-  type        = string
-  default = "10.0.1.0/24"
+  type        = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 variable "private_subnet_cidr_blocks" {
-   type        = string
-   default = "10.0.2.0/24"   
+   type        = list(string)
+   default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]  
 }
 variable "azs" {
  type        = list(string)
