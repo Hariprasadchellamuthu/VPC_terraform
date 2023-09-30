84828ab6eca01554d9b4f95d12e93e2f8542d42a
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-09-30 19:03:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_internet_gateway" "my_igw" {
 }
 
 resource "aws_subnet" "public_subnets" {
- count             = length(var.public_subnet_cidrs)
+ count             = var.public_subnet_count
  vpc_id            = aws_vpc.main.id
  cidr_block        = element(var.public_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index)
@@ -57,7 +57,7 @@ resource "aws_subnet" "public_subnets" {
 }
 
 resource "aws_subnet" "private_subnets" {
- count             = length(var.private_subnet_cidrs)
+ count             = var.private_subnet_count
  vpc_id            = aws_vpc.main.id
  cidr_block        = element(var.private_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index)
