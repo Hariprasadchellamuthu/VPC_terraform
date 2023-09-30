4cf51ffafd84ab462000e44c919aa326ce836625
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-09-30 18:22:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,15 @@ variable "vpc_cidr_block" {
   type        = string
   default = "10.0.0.0/16"
 }
-variable "public_subnet_count" {
-  type        = number
-  default = 2
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
-variable "private_subnet_count" {
-  type        = number
-  default = 2
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 variable "public_subnet_cidr_blocks" {
   type        = string
@@ -21,6 +23,11 @@ variable "public_subnet_cidr_blocks" {
 variable "private_subnet_cidr_blocks" {
    type        = string
    default = "10.0.2.0/24"   
+}
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -36,16 +43,23 @@ resource "aws_internet_gateway" "my_igw" {
 }
 
 resource "aws_subnet" "public_subnets" {
-  count = var.public_subnet_count
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.public_subnet_cidr_blocks
-  availability_zone = "us-east-1a" # Change to your desired availability zone
-  map_public_ip_on_launch = true
+ count             = length(var.public_subnet_cidrs)
+ vpc_id            = aws_vpc.main.id
+ cidr_block        = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
 }
 
 resource "aws_subnet" "private_subnets" {
-  count = var.private_subnet_count
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.private_subnet_cidr_blocks
-  availability_zone = "us-east-1b" # Change to your desired availability zone
+ count             = length(var.private_subnet_cidrs)
+ vpc_id            = aws_vpc.main.id
+ cidr_block        = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
 }
