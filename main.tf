7eaf2834b449b8e2c73374eabf24632eb96b1245
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-10-07 16:53:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ variable "vpc_cidr_block" {
 }
 
 variable "public_subnet_cidrs" {
-  type        = list(string)
+  type        = list
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-   type        = list(string)
+   type        = list
    default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
