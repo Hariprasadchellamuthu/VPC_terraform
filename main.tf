5d09f09435233dd42e95ace0da37451064c99051
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-10-07 21:53:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_internet_gateway" "ik" {
 resource "aws_subnet" "public_subnets" {
  count             = length(var.public_subnet_cidrs)
  vpc_id            = aws_vpc.my_vpc.id
- cidr_block        = element(var.public_subnet_cidrs, count.index)
+ cidr_block        = var.public_subnet_cidrs[count.index]
  availability_zone = element(var.azs, count.index)
  map_public_ip_on_launch = true
  
@@ -52,7 +52,7 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_subnet" "private_subnets" {
  count             = length(var.private_subnet_cidrs)
  vpc_id            = aws_vpc.my_vpc.id
- cidr_block        = element(var.private_subnet_cidrs, count.index)
+ cidr_block        = var.private_subnet_cidrs[count.index]
  availability_zone = element(var.azs, count.index)
  map_public_ip_on_launch = false 
  tags = {
